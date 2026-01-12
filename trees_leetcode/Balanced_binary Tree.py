5716f04b7c046d3cacbbf6453851de355ecc7afc
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-01-12 13:57:23 -0800

    Update Balanced_binary Tree.py

--- trees_leetcode/Balanced_binary Tree.py
+++ trees_leetcode/Balanced_binary Tree.py
@@ -1,1 +1,2 @@
 ''' A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.'''
+'''What does “balanced” mean : Each level splits nodes into two halves , Tree grows wide, not tall
