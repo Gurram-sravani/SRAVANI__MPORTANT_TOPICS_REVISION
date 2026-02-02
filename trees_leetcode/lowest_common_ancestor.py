65eded1fcc57b63cc53fd4f4ac712fb1fa8360df
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-02-02 09:49:04 -0800

    Create lowest_common_ancestor.py

---
+++ trees_leetcode/lowest_common_ancestor.py
@@ -0,0 +1,46 @@
+# BRUTE FORCE APPROACH:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        p_seen=[]
+        q_seen=[]
+        common_char=None
+        self.common(root,p,p_seen)
+        self.common(root,q,q_seen)
+        i=0
+        if len(p_seen)<len(q_seen):
+            while i<len(p_seen):
+                if p_seen[i]==q_seen[i]:
+                    common_char=p_seen[i]
+                i+=1
+        else:
+            while i<len(q_seen):
+                if p_seen[i]==q_seen[i]:
+                    common_char=q_seen[i]
+                i+=1
+        return common_char
+
+    def common(self,root,target,path):
+        if root is None:
+            return
+        path.append(root)
+        if root==target:
+            return True
+        left=self.common(root.left,target,path)
+        if left is True:
+            return True
+        right=self.common(root.right,target,path)
+        if right is True:
+            return True
+        path.pop()  # If I dont find the target in left and right the node needs to get deleted (backtrack starts here)
+        return False
+
+# Time Complexity: O(n) to find p_seen list +O(n) to find q_seen + O(n) to find common char (traversing list)
+#Space Complexity: O(n) (p_seen)+O(n) q_seen + O(n) recursion stack 
