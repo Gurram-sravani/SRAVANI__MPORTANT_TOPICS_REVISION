4d0e293a49a834ce91435cdcf693c46e3c7c634e
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-02-18 12:38:33 -0800

    Update Generate Parenthesis.py

--- Recursion/Generate Parenthesis.py
+++ Recursion/Generate Parenthesis.py
@@ -18,3 +18,28 @@ class Solution:
 
 
 # becareful when you are passing strings , check how string append works here when backtracking understand this concept more 
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        self.ans = []
+        self.dfs(open_left=n, close_left=n, new_str="")
+        return self.ans
+
+    def dfs(self, open_left: int, close_left: int, new_str: str) -> None:
+        # base case: used all parentheses
+        if open_left == 0 and close_left == 0:
+            self.ans.append(new_str)
+            return
+
+        # Option 1: add "(" if we still have opens left
+        if open_left > 0:
+            new_valid_str = new_str + "("
+            self.dfs(open_left - 1, close_left, new_valid_str)
+
+        # Option 2: add ")" only if it won't break validity
+        # Valid rule: we can close only if we have more closes left than opens left
+        # (meaning we have an unmatched "(" already placed)
+        if close_left > open_left:
+            new_valid_str = new_str + ")"
+            self.dfs(open_left, close_left - 1, new_valid_str)
