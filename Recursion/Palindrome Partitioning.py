be7e91d42686e8d8b04d18ca32fa3f6ec66b5526
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-02-18 23:04:10 -0800

    Create Palindrome Partitioning.py

---
+++ Recursion/Palindrome Partitioning.py
@@ -0,0 +1,4 @@
+Substring is a contigous , cannot skip any index, must be defind as [start:end+1]  => Substring logic = choose start and end
+subsequence  Does NOT need to be contiguous, can skip index, keep original order => Subsequence logic = take / skip pattern
+
+# Partitioning by every index for every string (so you need to do the partition for a string from start to end of the string).
