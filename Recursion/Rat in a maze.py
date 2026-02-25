2c452ed1a0e487455ddea89e4f9b4e0063cbd3d3
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-02-25 11:22:39 -0800

    Update Rat in a maze.py

--- Recursion/Rat in a maze.py
+++ Recursion/Rat in a maze.py
@@ -9,19 +9,20 @@ class Solution:
     visited=set()
     self.backtrack(spr=0,spc=0,grid,directions,path,output,n,visited)
     return output
-  def backtrack(self,spr,spc,grid,directions,path,output,n,visited)
+  def backtrack(self,0, 0, grid, directions, path, output, n, visited):
     if spr==n-1 and spc==n-1:
       output.append(path.copy())
       return
     path.append((spr,spc))
     visited.add((spr,spc))
     for (r,c) in directions:
-        spr=spr+r
-        spc=spc+c
-        if spr<0 or spc<0 or spr>=len(grid) or spc>=len(len(grid[0]) or (spr,spc) in visited or grid[spr][spc]==0:
+        nr = spr + r
+        nc = spc + c
+        if nr < 0 or nc < 0 or nr >= n or nc >= n or (nr, nc) in visited or grid[nr][nc] == 0:
           continue   ''' Dont use return becaus eif you use return That exits the entire backtrack call right away, so it won’t try the remaining directions.
 In backtracking, if one direction is invalid, you should skip it and continue, not stop everything.This will make you miss valid paths.'''
           
-          self.backtrack(spr,spc,grid,directions,path,output,n,visited)
+          self.backtrack(nr, nc, grid, directions, path, output, n, visited)
+    visited.remove((spr, spc))
     path.pop()
         
