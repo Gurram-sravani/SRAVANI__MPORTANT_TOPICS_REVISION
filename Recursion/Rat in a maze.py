519e898191a7f395beed5a2c02cbdd6d8ac2cc83
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-02-24 21:09:52 -0800

    Update Rat in a maze.py

--- Recursion/Rat in a maze.py
+++ Recursion/Rat in a maze.py
@@ -1,23 +1,27 @@
 class Solution:
   def rat_in_a_maze(self,n,grid):
+    if grid[0][0]==0
+      return 0
     n=len(grid)
     directions=[(1,0),(0,1),(-1,0),(0,-1)]
     path=[]
     output=[]
-    self.backtrack(spr=0,spc=0,grid,directions,path,output,n)
-  def self.backtrack(spr,spc,grid,directions,path,output,n)
-    if spr=n-1 and spc=n-1:
+    visited=set()
+    self.backtrack(spr=0,spc=0,grid,directions,path,output,n,visited)
+    return output
+  def backtrack(self,spr,spc,grid,directions,path,output,n,visited)
+    if spr==n-1 and spc==n-1:
       output.append(path.copy())
       return
-    path.append((r,c))
-    for r in range(len(grid)):
-      for c in range(len(grid[0]):
-        if r<0 or c<0 or r>=len(grid) or c>=len(len(grid[0]):
-          return
-        if grid[r][c]==0:
-          continue
-          spr=spr+r
-          spc=spc+c
-          self.backtrack(spr,spc,grid,directions,path,output,n)
+    path.append((spr,spc))
+    visited.add((spr,spc))
+    for (r,c) in directions:
+        spr=spr+r
+        spc=spc+c
+        if spr<0 or spc<0 or spr>=len(grid) or spc>=len(len(grid[0]) or (spr,spc) in visited or grid[spr][spc]==0:
+          continue   ''' Dont use return becaus eif you use return That exits the entire backtrack call right away, so it won’t try the remaining directions.
+In backtracking, if one direction is invalid, you should skip it and continue, not stop everything.This will make you miss valid paths.'''
+          
+          self.backtrack(spr,spc,grid,directions,path,output,n,visited)
           path.pop()
         
