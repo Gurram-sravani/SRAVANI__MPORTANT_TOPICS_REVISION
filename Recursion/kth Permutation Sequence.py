6ed9dbeb63e2193751bac07251f3a46344c61048
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-02-25 17:38:30 -0800

    Update kth Permutation Sequence.py

--- Recursion/kth Permutation Sequence.py
+++ Recursion/kth Permutation Sequence.py
@@ -1,3 +1,5 @@
+if you generate all the permutation and sort the output and get the kth permutation string will take more time 
+
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         path = []
@@ -40,3 +42,18 @@ class Solution:
 
 # time complexity : Recursion O(n!.n) 
 # Space Complexity : O(n) Recursion Stack Space + O(n) (path+used) + O(n) Output string =>O(n) 
+
+Optimised Version :  In optimized approach, for example n=4 =>[1,2,3,4] , k=17 If I take 0-based  indexing, k=17-1=> you need 16th Permutation 
+1+[2,3,4] => here I can form 6 permutations 3!=>6 (0-5) 
+2+[1,3,4] => (6-11) permutations
+3+[1,2,4] => (12-17) -> 16th Permutation lies in this bucket, but here you can form 6 permutations => 16%6=4 
+Finally you need to find 4th Permutation in this bucket our k becomes 4 mathematically (first digit - 3) 
+4+[1,2,3] => (18-23)
+
+Now if you take 3+[1,2,4]:
+1+[2,4] => again 2 permutations - [0-1] - 0
+2+[1,4] => 2 permutation - [2-3] - 1
+4+[1,2] => 2 permutations -[4-5] - 2 => k=4/2 =>2  (second digit is 4 => [3,4,-,-]
+Now my k becomes 4%2=0 , k=0  and the remaninign set is [1,2]
+1+ [2] => 1 permutation -[0]
+2+[1] => 1 permutation - [1] 
