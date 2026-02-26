ab77d1e2dba393170cd808817bd85b43d4e8ba5c
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-02-26 11:39:37 -0800

    Create Longest word with all prefixes.py

---
+++ Trie/Longest word with all prefixes.py
@@ -0,0 +1,10 @@
+class TrieNode:
+    def__init__(self):
+        self.children={}
+        self.is_end=False
+class Trie:
+  def __init(self,root):
+    self.root=TrieNode
+  def longestWord(self, words: List[str]) -> str:
+    curr=self.root
+    
