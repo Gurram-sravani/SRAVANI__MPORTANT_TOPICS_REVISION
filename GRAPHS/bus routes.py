4cef3a56a758ddfb823d466a68f4c2c7a81b4854
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-03-03 22:16:31 -0800

    Update bus routes.py

--- GRAPHS/bus routes.py
+++ GRAPHS/bus routes.py
@@ -28,3 +28,42 @@ class Solution:
 # let n be the total numbe rof buses and k be the total stops
 # Time Complexity: creating adj_list O(k) + while Loop O(n) => O(n+k) 
 # Space Complexity: adj_list O(k) + visited O(n)+ queue O(n) =>O(n+k)
+
+
+# Below I have added few optimizations 
+
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source==target:
+            return 0
+        adj_list=defaultdict(list)
+        for bus,stop in enumerate(routes):
+            for s in stop:
+                adj_list[s].append(bus)
+        if source not in adj_list:
+            return -1
+        queue=deque()
+        visited=set()
+        visited_stops=set()
+        level=0
+        for bus in adj_list[source]:
+            queue.append(bus)
+            visited.add(bus)
+        while queue:
+            size=len(queue)
+            for i in range(len(queue)):
+                bus=queue.popleft()
+                visited.add(bus)
+                for nxt_stop in routes[bus]:
+                    if nxt_stop==target:
+                        return level+1
+                    if nxt_stop not in visited_stops:
+                        for nxt_bus in adj_list[nxt_stop]:
+                            if nxt_bus not in visited:
+                                visited.add(nxt_bus)
+                                queue.append(nxt_bus)
+            level+=1
+        return -1
+# let n be the total numbe rof buses and k be the total stops
+# Time Complexity: creating adj_list O(k) + while Loop O(n) => O(n+k) 
+# Space Complexity: adj_list O(k) + visited O(n)+ queue O(n) =>O(n+k)
