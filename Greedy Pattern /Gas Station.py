57af17eec3957bdc8acb2c6396dc61cdd1b01f62
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-03-09 11:45:33 -0700

    Update Gas Station.py

--- Greedy Pattern /Gas Station.py
+++ Greedy Pattern /Gas Station.py
@@ -1,3 +1,19 @@
+Use prefix sum approach by checking tank=gas[i]-cost[i]+tank => whenever you get tank value negative it means that from the start index to current index - you cannot start  
+"net = [1,1,1,1,1,1,1,1,-10] // gas[i]-cost[i]
+Greedy scan:
+start=0
+tank accumulates until index 8
+at index 8 → tank becomes negative
+Now GREEDY says:
+👉 If start=0 failed at index 8
+👉 then start=1,2,3,4,5,6,7,8 will ALSO fail
+
+Why?
+Because starting later gives less accumulated gas before reaching index 8.
+So greedy skips all those starts and jumps directly to:
+start = 9"
+
+
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas)<sum(cost):
