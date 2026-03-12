491e1728fdaf8a4201ccf553e3149c386f09484c
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-03-12 09:49:02 -0700

    Update climbing stairs.py

--- DP/climbing stairs.py
+++ DP/climbing stairs.py
@@ -1,1 +1,23 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo={i:-1 for i in range(n+1)}
+        cnt=self.memo(memo,n)
+        return cnt
+    def memo(self,memo,i):
+        if i in memo and memo[i]!=-1:
+            return memo[i]
+        if i<=1:
+            return 1
+        memo[i]=self.memo(memo,(i-1))+self.memo(memo,(i-2))
+        return memo[i]
+# Time Complexity: O(n)
+# Space Complexity: O(n) stack Space + O(n) memo space 
 
+Optimized Version: 
+def climbStairs(n):
+    a,b = 1,1
+    for i in range(n-1):
+        a,b = b,a+b
+    return b
+# Time Complexity: O(n)
+# Space Complexity: O(1)
