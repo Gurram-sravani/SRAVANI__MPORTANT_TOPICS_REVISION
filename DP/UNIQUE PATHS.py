f9789a9a0cec4b34334267ae26cafe9c486b5bd6
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-03-13 16:05:42 -0700

    Create UNIQUE PATHS.py

---
+++ DP/UNIQUE PATHS.py
@@ -0,0 +1,19 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo=[[-1]*n for i in range(m)]
+        return self.paths(m, n, memo,r=0,c=0)
+
+    def paths(self,m,n,memo,r,c):
+        if r>=m or c>=n:
+            return 0
+        if r==m-1 and c==n-1:
+            return 1
+        if memo[r][c]!=-1:
+            return memo[r][c]
+        total=self.paths(m, n, memo, r, c+1)+self.paths(m, n, memo, r+1, c)
+        if r<m and c<n:
+            memo[r][c]=total
+        return total
+
+TIME COMPLEXITY: O(m+n) 
+SPACE COMPLEXITY : O(mn)
