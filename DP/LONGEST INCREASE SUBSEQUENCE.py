95cd5e08b3e4e14507ea821ca7ff30ad11a1ad7e
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-03-16 09:29:09 -0700

    Create LONGEST INCREASE SUBSEQUENCE.py

---
+++ DP/LONGEST INCREASE SUBSEQUENCE.py
@@ -0,0 +1,7 @@
+Subsequence: Any Contagious/nonContagious series of elemnts that follows the Order.
+Generate all subsequences and check whetehr they are in incresing order or not - BRUTE FORCE - 2^n 
+
+RECURRENCES: 
+1. Express everything in terms of index 
+2. Explore all possibilities in subsequences - Either you pick , or dont take
+3. Max length of both - MAx(Take, NotTake)
