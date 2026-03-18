55ce12483ba88fbe4f53be3289e036ebcb91f080
Gurram-sravani <48584585+Gurram-sravani@users.noreply.github.com>
2026-03-17 22:06:35 -0700

    Update Merge Sorted Array.py

--- Arrays/Merge Sorted Array.py
+++ Arrays/Merge Sorted Array.py
@@ -18,4 +18,48 @@ class Solution:
             k-=1
 # Time Complexity: O(m+n)
 #Space Complexity: O(1)
-        
+
+
+
+import math
+def merge(arr1, arr2):
+    n, m = len(arr1), len(arr2)
+    gap = math.ceil((n + m) / 2)
+
+    while gap > 0:
+        i = 0
+        j = gap
+
+        while j < (n + m):
+            
+            # Case 1: both pointers in arr1
+            if i < n and j < n:
+                if arr1[i] > arr1[j]:
+                    arr1[i], arr1[j] = arr1[j], arr1[i]
+
+            # Case 2: i in arr1, j in arr2
+            elif i < n and j >= n:
+                if arr1[i] > arr2[j - n]:
+                    arr1[i], arr2[j - n] = arr2[j - n], arr1[i]
+
+            # Case 3: both in arr2
+            else:
+                if arr2[i - n] > arr2[j - n]:
+                    arr2[i - n], arr2[j - n] = arr2[j - n], arr2[i - n]
+
+            i += 1
+            j += 1
+
+        if gap == 1:
+            gap = 0
+        else:
+            gap = math.ceil(gap / 2)
+
+# Example
+arr1 = [1, 4, 7, 8, 10]
+arr2 = [2, 3, 9]
+
+merge(arr1, arr2)
+
+print(arr1)
+print(arr2)
